3fc71bd7a0bd3f92d284f84485783d634a58c4fe
sampritavh <hegde.samprita@gmail.com>
2013-07-11 22:40:12 +0530

    Ignore empty tweets

--- TweetSentimentMapReduce/src/com/flux7/tweetsentiment/mapreduce/TweetSentimentMapper.java
+++ TweetSentimentMapReduce/src/com/flux7/tweetsentiment/mapreduce/TweetSentimentMapper.java
@@ -6,48 +6,62 @@ import org.apache.hadoop.io.*;
 
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Mapper.Context;
+import org.json.simple.parser.ParseException;
+import org.mortbay.log.Log;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 
 import com.flux7.tweetsentiment.TweetScorer;
 
 public class TweetSentimentMapper extends
 
-    
-    Mapper<LongWritable, Text, IntWritable,TweetScoreWrapper>{
+Mapper<LongWritable, Text, IntWritable, TweetScoreWrapper> {
+  
+  private static final Logger LOGGER = LoggerFactory.getLogger(TweetSentimentMapper.class);
 
-  private TweetScorer scorer = new TweetScorer( "AFINN-111.txt");
+  private TweetScorer scorer = new TweetScorer("AFINN-111.txt");
   private static final Float TWEET_LOWER_BOUNDARY_SCORE = -2.0F;
-  
+
   private static final Float TWEET_UPPER_BOUNDARY_SCORE = 2.0F;
-  
-  
-  @Override 
-  public void map( LongWritable key, Text value, 
-            Context context) throws IOException, InterruptedException{
+
+  @Override
+  public void map(LongWritable key, Text value, Context context)
+      throws IOException, InterruptedException {
     String text = value.toString();
-    
-    if( text.length() > 0){
-      String tweetText = scorer.getTweetText(text);
-      float tweetScore = scorer.getScore(tweetText);
-      IntWritable bucket = null;
-      Float score = new Float(tweetScore);
-    int cmp1 = score.compareTo(TWEET_LOWER_BOUNDARY_SCORE);
-    if( cmp1 < 0 ){
-      bucket = new IntWritable(0);
-    }
-    else  {
-      int cmp2 = score.compareTo(TWEET_UPPER_BOUNDARY_SCORE);
-      if( cmp2 >= 0){
-        bucket = new IntWritable(2);
+
+    if (text.length() > 0) {
+      String tweetText;
+      try {
+        tweetText = scorer.getTweetText(text);
+        float tweetScore = scorer.getScore(tweetText);
+        IntWritable bucket = null;
+        Float score = new Float(tweetScore);
+        int cmp1 = score.compareTo(TWEET_LOWER_BOUNDARY_SCORE);
+        if (cmp1 < 0) {
+          bucket = new IntWritable(0);
+        } else {
+          int cmp2 = score.compareTo(TWEET_UPPER_BOUNDARY_SCORE);
+          if (cmp2 >= 0) {
+            bucket = new IntWritable(2);
+          } else {
+            bucket = new IntWritable(1);
+          }
+        }
+        if( tweetText != null){
+          TweetScoreWrapper tweetScoreText = new TweetScoreWrapper(tweetText, tweetScore);
+        context.write(bucket, tweetScoreText);
+        }
+        else{
+          Log.warn( "TweetText is null!!!!!");
+        }
+        
+      } catch (ParseException e) {
+        LOGGER.error("Exception", e);
       }
-      else{
-        bucket = new IntWritable(1);
-      }
+
     }
-      TweetScoreWrapper tweetScoreText = new TweetScoreWrapper( tweetText,tweetScore);
-    context.write(bucket,tweetScoreText);
-    }
-    
+
   }
-  
 
 }
